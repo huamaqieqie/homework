6452b84480cf09044de9fc3b02bcfd691d431549
huamaqieqie <huamaqie@gmail.com>
2025-12-25 12:42:00 +0800

    提交库测试

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,2 +1,16 @@
-print("test")
-print("dd,zjm")
+from scipy.optimize import fsolve, root
+import numpy as np
+r=3.3
+# 解单个方程
+def equation(x):
+    return 23/r+29/x+4*x/(r**2)-17.5
+
+def equation1(y):
+    return (3.3**2)*3.3*(6*3.3+4*y)*((12*np.pi)**2)-1
+
+initial_guess = 1
+solution = fsolve(equation, initial_guess)
+print(f"数值解：{solution[0]:.6f}")
+
+solution2 = fsolve(equation1, initial_guess)
+print(f"数值解：{solution2[0]:.6f}")
